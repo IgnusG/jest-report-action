f6e4b9d7d6024479b913049327c44cab95edf1c2
IgnusG <IgnusG@users.noreply.github.com>
2020-04-06 17:44:54 +0200

    Add more formatting to summary and annotations
    Such as stacktrace cleaning and markdown highlights.

--- tasks.js
+++ tasks.js
@@ -9,6 +9,7 @@ import * as t from '@babel/types';
 import escapeStringRegexp from 'escape-string-regexp';
 
 import { promises as fs } from 'fs';
+import path from 'path';
 
 
 export async function readAndParseXMLFile(file, { $fs = fs, $xmlParser = xmlParser } = {}) {
@@ -111,15 +112,79 @@ export function findTestIn(ast, { $traverse = traverse } = {}) {
   }
 }
 
-export function createAnnotation({ path }, testcase, location) {
+function cleanStackWithRelativePaths(stacktrace) {
+  const filters = [
+    line => {
+      // Remove jasmin stacks
+      if (line.includes('node_modules/jest-jasmin')) return false;
+      // Remove process queue stacks
+      if (line.includes('internal/process/task_queues')) return false;
+      // Remove empty promises
+      // eslint-disable-next-line no-useless-escape
+      if (line.trimStart() === 'at new Promise (\<anonymous\>)') return false;
+
+      return line;
+    },
+    line => {
+      const { groups: { file } } = (/.*\((?<file>.*)\).?/).exec(line) || { groups: { file: false } };
+
+      return file 
+        ? line.replace(file, path.relative(process.cwd(), file)) 
+        : line;
+    }
+  ];
+
+  const applyFilters = line => filters.reduce((result, filter) => filter(result), line);
+  const isNotEmpty = line => line !== false;
+
+
+  return stacktrace
+    .map(applyFilters)
+    .filter(isNotEmpty);
+}
+
+function formatJestMessage(message) {
+  const messageLines = message.split('\n');
+
+  // Skip first line (title) and one blank line
+  const expectationStart = 2; 
+  const filterStacktrace = line => line.trimStart().startsWith('at ');
+
+  try {
+    const [ title ] = messageLines;
+
+    const expectations = messageLines
+      .slice(expectationStart)
+      .filter(line => !filterStacktrace(line))
+      .join('\n');
+
+    const stacktrace = messageLines.filter(filterStacktrace);
+
+    return {
+      title,
+      expectations,
+      stacktrace: cleanStackWithRelativePaths(stacktrace).join('\n')
+    }
+  } catch(error) {
+    console.error(`Failed to parse - falling back to "stupid" mode - error: ${ error.message }`);
+
+    return message;
+  }
+}
+
+export function createAnnotation({ path: filePath }, testcase, location) {
   const { failure: [ message ] } = testcase;
 
+  const { title, expectations, stacktrace } = formatJestMessage(message);
+
   let annotation = {
-    path,
+    path: filePath,
+    title,
     start_line: location.start.line,
     end_line: location.end.line,
     annotation_level: 'failure',
-    message
+    message: expectations,
+    rawValue: stacktrace
   };
 
   if (location.start.line === location.end.line) {
@@ -158,7 +223,7 @@ export async function createAnnotationsFromTestsuites(testsuites) {
 }
 
 export async function createCheckRunWithAnnotations(checkInformation, { $github = github, $config }) {
-  const { time, passed, failed, annotations } = checkInformation;
+  const { time, passed, failed, total, annotations } = checkInformation;
 
   const octokit = new $github.GitHub($config.accessToken);
 
@@ -170,7 +235,7 @@ export async function createCheckRunWithAnnotations(checkInformation, { $github 
       title: 'Jest Test Results',
       summary: `
 ## These are all the test results I was able to find from your jest-junit reporter
-${ passed + failed } tests were completed in ${ time }s with ${ passed } passed ✔ and ${ failed } failed ❌ tests.
+**${ total }** tests were completed in **${ time }s** with **${ passed }** passed ✔ and **${ failed }** failed ✖ tests.
 `,
       annotations
     }
