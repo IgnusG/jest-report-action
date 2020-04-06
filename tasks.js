efa60591a2fd29511dff0791aef8696dfe941037
IgnusG <IgnusG@users.noreply.github.com>
2020-04-06 18:29:56 +0200

    Adjust formatting of strings to improve readability of reports

--- tasks.js
+++ tasks.js
@@ -159,15 +159,11 @@ function formatJestMessage(message) {
       .join('\n');
 
     const stacktrace = messageLines.filter(filterStacktrace);
-    const [ firstLineOfStacktrace ] = stacktrace;
-    const stringStart = 0;
-    const startsWith = firstLineOfStacktrace
-      .slice(stringStart, firstLineOfStacktrace.indexOf('at '));
 
     return {
       title,
       expectations,
-      stacktrace: `${ startsWith }Stacktrace:\n${ cleanStackWithRelativePaths(stacktrace).join('\n') }`
+      stacktrace: `Stacktrace:\n${ cleanStackWithRelativePaths(stacktrace).join('\n') }`
     }
   } catch(error) {
     console.error(`Failed to parse - falling back to "stupid" mode - error: ${ error.message }`);
@@ -272,7 +268,7 @@ export async function publishTestResults(testInformation, { $github = github, $c
 
   await createCheckWithAnnotations({
     summary:
-      '## These are all the test results I was able to find from your jest-junit reporter' +
+    '#### These are all the test results I was able to find from your jest-junit reporter\n' +
       `**${ total }** tests were completed in **${ time }s** with **${ passed }** passed ✔ and **${ failed }** failed ✖ tests.`,
     conclusion,
     annotations: annotations.slice(zeroAnnotations, maximumAnnotations)
