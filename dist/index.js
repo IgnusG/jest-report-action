a1f31bc6803c25082cf84438f60300d99f0092a2
IgnusG <IgnusG@users.noreply.github.com>
2020-04-06 15:30:08 +0200

    Build latest version 1.3.2

--- dist/index.js
+++ dist/index.js
@@ -78032,6 +78032,12 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 async function readAndParseXMLFile(file, {
   $fs = _fs.promises,
   $xmlParser = xmlParser
@@ -78218,7 +78224,8 @@ async function createCheckRunWithAnnotations(checkInformation, {
     annotations
   } = checkInformation;
   const octokit = new $github.GitHub($config.accessToken);
-  const checkRequest = {
+
+  const checkRequest = _objectSpread({}, $github.context.repo, {
     name: 'Jest Test',
     head_sha: $github.context.sha,
     status,
@@ -78230,7 +78237,7 @@ ${passed + failed} tests were completed in ${time}s with ${passed} passed ✔ an
 `,
       annotations
     }
-  };
+  });
 
   try {
     await octokit.check.create(checkRequest);
