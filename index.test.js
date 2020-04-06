f685496d3d0517cd80e963d5ee90fc5665242418
IgnusG <IgnusG@users.noreply.github.com>
2020-04-06 12:42:27 +0200

    Write manual test (simply run node -r esm ./index.test.js)
    You need a generated junit.xml and the source files (exact structure).

---
+++ index.test.js
@@ -0,0 +1,13 @@
+import { readAndParseXMLFile, parseTestsuite, createAnnotationsFromTestsuites } from './tasks';
+
+async function testIntegration() {
+  const { testsuites: jest } = await readAndParseXMLFile('junit.xml');
+  const testsuites = jest.testsuite.map(parseTestsuite);
+
+  const annotations = await createAnnotationsFromTestsuites(testsuites);
+
+  console.log(annotations);
+}
+
+testIntegration().catch(error => console.error(error));
+
