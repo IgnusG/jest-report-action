b18c788a066b5f1e66ca4abf656af6b687d59a80
IgnusG <IgnusG@users.noreply.github.com>
2020-04-06 15:23:24 +0200

    Build latest version 1.3.1

--- dist/index.js
+++ dist/index.js
@@ -78249,10 +78249,6 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
 const config = {
   accessToken: core.getInput('access-token'),
   junitFile: core.getInput('junit-file'),
@@ -78262,17 +78258,14 @@ const config = {
 async function parseTestsAndCreateJestCheck({
   $config = config
 } = {}) {
-  const _await$readAndParseXM = await (0, _tasks.readAndParseXMLFile)($config.junitFile),
-        {
+  const {
     testsuites: jest
-  } = _await$readAndParseXM,
-        testSuiteRoot = _objectWithoutProperties(_await$readAndParseXM, ["testsuites"]);
-
+  } = await (0, _tasks.readAndParseXMLFile)($config.junitFile);
   const {
     time,
     tests,
     failures
-  } = (0, _tasks.parseTestInformation)(testSuiteRoot);
+  } = (0, _tasks.parseTestInformation)(jest);
   const testsuites = jest.testsuite.map(_tasks.parseTestsuite);
   const annotations = await (0, _tasks.createAnnotationsFromTestsuites)(testsuites);
   const checkInformation = {
